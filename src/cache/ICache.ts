5a6b224bbd4ffdae95ce35b1aa49557670a5b7f5
AnkitKTrivedi <38967951+AnkitKTrivedi@users.noreply.github.com>
2026-07-14 22:26:20 +0530

    refactore

--- src/cache/ICache.ts
+++ src/cache/ICache.ts
@@ -7,12 +7,3 @@ export interface ICache<T> {
 
   clear(): Promise<void>;
 }
-export interface ICache<T> {
-  get(key: string): Promise<T | undefined>;
-
-  set(key: string, value: T): Promise<void>;
-
-  delete(key: string): Promise<void>;
-
-  clear(): Promise<void>;
-}
